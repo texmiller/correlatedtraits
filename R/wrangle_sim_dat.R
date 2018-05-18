fd3da359669e0991d47935d08f0bc99a35749394
Thomas E Miller <tm9@ad.rice.edu>
2018-05-18 18:06:39 -0500

    Updated sim wrangle to include gen 1 so that we can calculate trait change between gens 1 and 20.

--- R/wrangle_sim_dat.R
+++ R/wrangle_sim_dat.R
@@ -6,7 +6,8 @@
 ##set wd to my local drive where I downloaded output files
 setwd("D:/ct_simulations/output/complete")
 # Random question for brad: Why are there different numnbers of replicates for each treatment (1043, 1008, 1040, etc.).
-# I will only use the first thousand, but this is odd.
+# I will only use the first thousand, but this is odd. -- ANSWER: I think it's because some simulations did not work
+# which is why there are missing numbers in the output list (hence tryCatch in the loop)
 library(stringr)
 library(tidyverse)
 
@@ -31,13 +32,15 @@ for(i in 1:length(h2_trts)){
         read.csv(paste0("ct_",h2_trts[i],"_",var_trts[j],"_",str_pad(k,4,pad = "0"),".csv"),header=F),
         error=function(e) e
       )
+      #some of the numbers that we are looping over do not exist as data files, not sure why.
+      #We are just skipping over this when it happens. It means final sample sizes will actually be <1000
       if(inherits(sim_out, "error")) next
 
       names(sim_out)[c(8,9,10,14,1)] <- c("rho_G","rho_E","gen","rightmost_dat","dat_names")
 
       hold <- sim_out %>%
-        filter(gen==20) %>%
-        select(rho_G,rho_E,dat_names,rightmost_dat) %>%
+        filter(gen==1 | gen==20) %>%
+        select(rho_G,rho_E,gen,dat_names,rightmost_dat) %>%
         spread(key = dat_names, value = rightmost_dat) %>%
         mutate(h2 = h2_trts[i],
                P_var = var_trts[j])
